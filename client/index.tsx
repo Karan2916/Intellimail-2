22254f563631fc83b5c6e852fb2bda4c0cde59b5
Karan2916 <kadyakaranbalaji@gmail.com>
2025-08-05 21:32:45 +0530

    Fix: Correct import order in index.tsx to resolve styling issues

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,8 @@
 
+import './src/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./src/App";
-import './src/index.css';
 
 
 const rootElement = document.getElementById('root');
